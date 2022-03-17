882beccd8c4727f8919ace738f925e9895749bcb
claytonleonardcook <claytonleonardcook@gmail.com>
2022-03-16 21:48:40 -0400

    test deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import sum from './sum';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          {sum(28, 36)}
         </p>
         <a
           className="App-link"
